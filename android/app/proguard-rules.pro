e67eafacb31ce9b5cbd7551da7da516cf361af22
junior90h <junior90h@gmail.com>
2023-05-04 14:01:07 +0200

    update

--- android/app/proguard-rules.pro
+++
@@ -1,2 +0,0 @@
--keep class com.dexterous.** { *; }
--keep class com.github.chinloyal.pusher_client.** { *; }
--- android/app/src/main/kotlin/com/hytechsolar/gizmo/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.hytechsolar.gizmo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
